6ed84c647f8bccbc11adb791ecd6e74c77846722
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-17 14:42:17 +0100

    taak 1 af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Adam";
+$student['achternaam'] = "Rijnbeek";
+$student['leeftijd'] = "16";
+$student['klas'] = "1D";
+ 
+echo "Gegevens Adam: Voornaam:". " " . " " . $student['voornaam'] . " " . "Achternaam:" . $student['achternaam'] . " " . "Leeftijd:" . " " . $student['leeftijd'] . " ". "Klas:" . " " . $student['klas'];
+?> 
