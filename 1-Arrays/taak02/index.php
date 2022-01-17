57562fc63a24635cb0e3f09e127c7b9a83cd17f2
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-17 14:54:01 +0100

    taak 2 af

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,8 @@
+<?php
+$persoon["voornaam"]   = "Willem Alexander";
+$persoon["achternaam"] = "Van Oranje";
+$persoon["opleiding"]  = "Software Ontwikkeling.";
+$persoon["leeftijd"]   = 53;
+
+echo $persoon["voornaam"]." ".$persoon["achternaam"]." is ".$persoon["leeftijd"]." en zit op de opleiding ".$persoon["opleiding"];
+?>  
