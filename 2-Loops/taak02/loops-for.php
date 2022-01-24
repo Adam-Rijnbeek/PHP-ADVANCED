2be9b30d51405bcc80b246e15d79bc216a369928
Adam <adam.rijnbeek@student.rocva.nl>
2022-01-24 14:36:20 +0100

    taak 02 af

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<ul>";
+for($leeftijd = 0; $leeftijd < 18; $leeftijd++){
+     echo "<li> ik ben $leeftijd jaar, dus ik heb geen stemrecht. </li>";
+}
+    echo "<li> Ik ben 18 jaar oud, dus ik heb stemrecht! </li>";
+echo "</ul>"
+
+?>
