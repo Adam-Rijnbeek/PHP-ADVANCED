cde73303e1ff0115c7b6ee74b56d0edb032dcabf
Adam <adam.rijnbeek@student.rocva.nl>
2022-02-07 14:53:57 +0100

    taak 05 af

---
+++ 2-Loops/taak05/loops-foreach-deel3.php
@@ -0,0 +1,12 @@
+<?php
+$students = [
+  'voornaam'   =>"Jan",
+  'achternaam' =>"Janssen",
+  'klas'       =>"9A",
+  'Leeftijd'   =>17,
+  'Woonplaats' =>"Amstelveen"
+];
+foreach($students as $key => $value){
+    echo $key . ": " . $value . "<br>";
+}
+?>
