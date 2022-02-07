e6ff5c15b6fe768407883cfd4023cb153a3c7b67
Adam <adam.rijnbeek@student.rocva.nl>
2022-02-07 14:11:48 +0100

    taak 03 af

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<ul>";
+$dagen = ['maandag' , 'dinsdag' , 'woensdag' , 'donderdag' , 'vrijdag' , 'zaterdag' , 'zondag'];
+foreach($dagen as $dag)
+echo "<li> $dag  <br> </li>";
+echo "</ul>";
+    
+?>
