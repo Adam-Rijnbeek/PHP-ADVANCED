4ce8e7df59f4d9860c5f0f9818cda59799cdfc68
Adam <adam.rijnbeek@student.rocva.nl>
2022-02-08 09:50:43 +0100

    taak 01 af

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,9 @@
+<?php
+echo "je hebt zojuist het volgende ingevoerd:". "<br>" . $_GET['first-name'] . "<br>";
+echo $_GET['last-name'] . "<br>";
+echo $_GET['klas'] . "<br>";
+echo $_GET['age'] . "<br>";
+echo $_GET['adres'] . "<br>";
+echo $_GET['placename'] . "<br>";
+echo $_GET['favemusicband'] . "<br>";
+?> 
