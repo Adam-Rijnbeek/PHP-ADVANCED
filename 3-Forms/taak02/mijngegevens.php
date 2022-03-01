12db7039dcd77308c4a831585156a95afcb7d10c
Adam <adam.rijnbeek@student.rocva.nl>
2022-03-01 09:09:42 +0100

    taak 02 verbeterd

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 echo "Dit heb je zojuist getypt:". $_POST["first-name"];
 foreach($_POST as $key => $value){
@@ -5,4 +15,9 @@ foreach($_POST as $key => $value){
 }
 
 echo "<body style='background-color:".$_POST ["kleur"]."'>";
-?>
+?>  
+ 
+</body>
+</html>
+
+
